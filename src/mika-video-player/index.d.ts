9ecd9f8002702c312e8ef5d91fdd58f2e843248e
式部 <2112952452@qq.com>
2024-05-03 22:42:22 +0800

    fix

--- src/mika-video-player/index.d.ts
+++
@@ -1,8 +0,0 @@
-export {default} from './dist/VideoPlayer';
-export * from './dist/Controller';
-
-export {Danmaku, DanmakuScheduler} from './dist/Danmaku';
-export {Debugger} from './dist/Debugger';
-
-export type {DanmakuAttr} from './dist/Danmaku';
-export type {VideoPlayerProps} from './dist/VideoPlayer.tsx';
--- src/mika-video-player/index.js
+++
@@ -1,5 +0,0 @@
-export {default} from './dist/VideoPlayer';
-export * from './dist/Controller';
-
-export {Danmaku, DanmakuScheduler} from './dist/Danmaku';
-export {Debugger} from './dist/Debugger';
