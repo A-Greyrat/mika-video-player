6d7b93833f58094a5ab4f1ad3ab0a8b808a652f3
式部 <2112952452@qq.com>
2024-05-03 22:28:57 +0800

    fix

--- src/mika-video-player/index.d.ts
+++ src/mika-video-player/index.d.ts
@@ -5,4 +5,4 @@ export {Danmaku, DanmakuScheduler} from './dist/Danmaku';
 export {Debugger} from './dist/Debugger';
 
 export type {DanmakuAttr} from './dist/Danmaku';
-export type {VideoPlayerProps} from './dist/VideoPlayer.tsx';
+export type {VideoPlayerProps} from './dist/VideoPlayer';
